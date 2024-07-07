22ceac8e5ab728d0468e1ea8027884d83d4998f2
ToanNguyenThai <19522366@gm.uit.edu.vn>
2024-07-07 21:58:24 +0700

    feat: use const for msg

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -7,6 +7,7 @@ import {
 import { RequestBody } from "@types";
 import { UnauthorizedException } from "@exceptions/error-handler";
 import { User } from "@prisma/client";
+import { AUTH_ERRORS } from "@constants/message";
 
 export const generateAccessToken = async (id: number) => {
     const token = jwt.sign(
@@ -40,21 +41,21 @@ export const verifyAuthorizationHeader = async (
 ) => {
     const authorization = req.header("authorization");
     if (!authorization) {
-        throw new UnauthorizedException("Unauthorized");
+        throw new UnauthorizedException(AUTH_ERRORS.UNAUTHORIZED);
     }
 
     const token = authorization.replace("Bearer ", "");
     if (!token) {
-        throw new UnauthorizedException("Unauthorized");
+        throw new UnauthorizedException(AUTH_ERRORS.UNAUTHORIZED);
     }
 
     try {
         const payload = await verifyToken(token, tokenSecret);
         if (!payload) {
-            throw new UnauthorizedException("Unauthorized");
+            throw new UnauthorizedException(AUTH_ERRORS.INVALID_TOKEN);
         }
         return payload;
     } catch (error) {
-        throw new UnauthorizedException("Unauthorized");
+        throw new UnauthorizedException(AUTH_ERRORS.UNAUTHORIZED);
     }
 };
