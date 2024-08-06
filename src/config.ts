e7dbeafa3fcf70aae391bc782966a68c88f93eb7
ToanNguyenThai <thaitoan3039015@gmail.com>
2024-08-06 10:58:29 +0700

    fix: allow local working

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,9 @@
 import dotenv from "dotenv";
 
-dotenv.config({ path: ".env" });
+dotenv.config({ path: ".env.local" });
 
 export const config = {
   PORT: process.env.PORT || 3000,
   JWT_SECRET: process.env.JWT_SECRET || "",
   REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET || "",
-  DATABASE_URL: process.env.DATABASE_URL || "",
 };
