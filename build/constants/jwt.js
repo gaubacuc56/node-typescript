1dd3b06623d649e319d3f442e8cf9487ddbe2bfc
ToanNguyenThai <thaitoan3039015@gmail.com>
2024-08-25 19:48:27 +0700

    fix: remove env

--- build/constants/jwt.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.REFRESH_TOKEN_EXPIRATION = exports.ACCESS_TOKEN_EXPIRATION = void 0;
-exports.ACCESS_TOKEN_EXPIRATION = "1m";
-exports.REFRESH_TOKEN_EXPIRATION = "7d";
