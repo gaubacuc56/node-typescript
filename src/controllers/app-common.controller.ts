7e6dba204be2a2c8c1eace58aa7bfc2e4642b98b
ToanNguyenThai <thaitoan3039015@gmail.com>
2024-09-01 17:25:51 +0700

    remove pipeline on pr create

--- src/controllers/app-common.controller.ts
+++ src/controllers/app-common.controller.ts
@@ -2,6 +2,6 @@ import { Response } from "express";
 
 export class AppCommonController {
   public healthCheck(_req: Request, res: Response) {
-    res.json("App is running");
+    res.json("App is working");
   }
 }
